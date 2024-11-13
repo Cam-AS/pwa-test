6c9d2707558a35ee0561bd1907780066406c9334
Cameron Kirkpatrick <cameronk@appstrax.tech>
2024-11-13 17:30:33 +0200

    Removed dead and bump version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { SwUpdate } from '@angular/service-worker';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'pwa-test';
-
-  constructor(private swUpdate: SwUpdate) {}
-
-  ngOnInit() {
-    if (this.swUpdate.isEnabled) {
-      this.swUpdate.versionUpdates.subscribe((event) => {
-        if (event.type === 'VERSION_READY') {
-          this.clearCachesAndReload();
-        }
-      });
-    }
-  }
-
-  private clearCachesAndReload() {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.getRegistrations().then((registrations) => {
-        registrations.forEach((registration) => {
-          registration.unregister();
-        });
-      });
-    }
-
-    // Delete all caches
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return Promise.all(
-          cacheNames.map((cacheName) => caches.delete(cacheName))
-        );
-      })
-      .then(() => {
-        // After clearing caches, reload the page to get the latest assets
-        window.location.reload();
-      });
-  }
 }
